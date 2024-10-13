6e37fda8752b458157fb3c56d86a4c8a3b6419a7
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-13 22:14:17 +0530

    feat: create `Enumerator` macro class

--- packages/enumerator_macro/lib/src/enumerator_macro_base.dart
+++ packages/enumerator_macro/lib/src/enumerator_macro_base.dart
@@ -1,4 +1,122 @@
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'dart:async';
+
+import 'package:macros/macros.dart';
+
+macro class Enumerator implements ClassDeclarationsMacro {
+  const Enumerator();
+
+  @override
+  Future<void> buildDeclarationsForClass(
+    ClassDeclaration clazz,
+    MemberDeclarationBuilder builder,
+  ) async {
+    final name = clazz.identifier.name;
+    final fieldList = await builder.fieldsOf(clazz);
+    // TODO(albin): Update condition
+    final fields = fieldList.where((e) {
+      return e.identifier.name != '_self' && e.identifier.name != 'values';
+    }).toList();
+
+    _buildBoolGetters(builder, fields);
+
+    _buildFromNameConstructor(builder, name);
+    _buildFromNameOrNullMethod(builder, name);
+
+    _buildFromIndexConstructor(builder, name);
+    _buildFromIndexOrNullMethod(builder, name);
+  }
+
+  void _buildFromIndexConstructor(
+    MemberDeclarationBuilder builder,
+    String name,
+  ) {
+    builder.declareInType(
+      DeclarationCode.fromParts([
+        '  factory $name.fromIndex(int index) {\n',
+        '    return values.firstWhere(\n',
+        '      (e) => e.index == index,\n',
+        '    );\n',
+        '  }\n',
+      ]),
+    );
+  }
+
+  void _buildFromIndexOrNullMethod(
+    MemberDeclarationBuilder builder,
+    String name,
+  ) {
+    builder.declareInType(
+      DeclarationCode.fromParts([
+        '  static $name? fromIndexOrNull(int? index) {\n',
+        '    if (index == null) return null;\n',
+        '    try {\n',
+        '      return $name.fromIndex(index);\n',
+        '    } catch (_) {\n',
+        '      return null;\n',
+        '    }\n',
+        '  }\n',
+      ]),
+    );
+  }
+
+  void _buildFromNameOrNullMethod(
+    MemberDeclarationBuilder builder,
+    String name,
+  ) {
+    builder.declareInType(
+      DeclarationCode.fromParts([
+        '  static $name? fromNameOrNull(String? name) {\n',
+        '    if (name == null) return null;\n',
+        '    try {\n',
+        '      return $name.fromName(name);\n',
+        '    } catch (_) {\n',
+        '      return null;\n',
+        '    }\n',
+        '  }\n',
+      ]),
+    );
+  }
+
+  void _buildFromNameConstructor(
+    MemberDeclarationBuilder builder,
+    String name,
+  ) {
+    builder.declareInType(
+      DeclarationCode.fromParts([
+        '  factory $name.fromName(String name) {\n',
+        '    return values.firstWhere(\n',
+        '      (e) => e.name == name,\n',
+        '    );\n',
+        '  }\n',
+      ]),
+    );
+  }
+
+  void _buildBoolGetters(
+    MemberDeclarationBuilder builder,
+    List<FieldDeclaration> fields,
+  ) {
+    builder.declareInType(
+      DeclarationCode.fromParts([
+        for (final f in fields) ...[
+          '  bool get is${f.identifier.name.cap} => _self == ${f.identifier.name};\n\n',
+        ],
+      ]),
+    );
+  }
+}
+
+extension on Builder {
+  void log(Object? message) {
+    report(
+      Diagnostic(
+        DiagnosticMessage(message.toString()),
+        Severity.info,
+      ),
+    );
+  }
+}
+
+extension on String {
+  String get cap => '${this[0].toUpperCase()}${substring(1)}';
 }
