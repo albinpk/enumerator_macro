3cb2181bfdf63de282d8da24f6c71a844020cfea
Albin <56157868+albinpk@users.noreply.github.com>
2024-10-13 17:34:08 +0530

    feat: create package template for `enumerator_macro`

---
+++ packages/enumerator_macro/example/enumerator_macro_example.dart
@@ -0,0 +1,6 @@
+import 'package:enumerator_macro/enumerator_macro.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/enumerator_macro/lib/enumerator_macro.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/enumerator_macro_base.dart';
